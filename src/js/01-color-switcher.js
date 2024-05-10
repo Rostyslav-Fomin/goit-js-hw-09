44ee312cebcf430c7890097808f2d1936986615d
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-05-10 09:31:54 +0300

    corrected task #1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', startBodyChangeColor);
+refs.stopBtn.addEventListener('click', stopBodyChangeColor);
+
+function startBodyChangeColor(e) {
+  timerId = setInterval(changeBodyColor, 1000);
+  e.target.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopBodyChangeColor(e) {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  e.target.disabled = true;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+const refs = {
+  dataInput: document.querySelector('input[type="text"]'),
+  timerContiner: document.querySelector('.timer'),
+}; 
+
+
