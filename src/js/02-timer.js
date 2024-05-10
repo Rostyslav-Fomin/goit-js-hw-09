75e5843480e0d80cc5653571bfe46a24f4d04a27
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-05-10 09:54:13 +0300

    some work in task #2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,9 +2,18 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const refs = {
   dataInput: document.querySelector('input[type="text"]'),
-  timerContiner: document.querySelector('.timer'),
+  timerValueDays: document.querySelector('.value[data-days]'),
+  timerValueHours: document.querySelector('.value[data-hours]'),
+  timerValueMinutes: document.querySelector('.value[data-minutes]'),
+  timerValueSeconds: document.querySelector('.value[data-minutes]'),
 }; 
 
-
+console.log(refs.timerValueDays)
