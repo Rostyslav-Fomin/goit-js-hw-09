fef369a933d1f7618fdc3f8002c86514e2d82a51
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-05-10 10:01:11 +0300

    corrected task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,6 +9,8 @@ const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
 };
+let timerId = null;
+
 
 refs.startBtn.addEventListener('click', startBodyChangeColor);
 refs.stopBtn.addEventListener('click', stopBodyChangeColor);
